700dd459596f64812bf563d3517bdec0d653f37f
l6_pc2 <l6_pc2@WIN-IM38HI1GTD6>
2021-11-11 09:25:46 +0300

    Add project files.

---
+++ Adam Asmaca/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OyunaBasla_btn = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // OyunaBasla_btn
+            // 
+            this.OyunaBasla_btn.Location = new System.Drawing.Point(544, 38);
+            this.OyunaBasla_btn.Name = "OyunaBasla_btn";
+            this.OyunaBasla_btn.Size = new System.Drawing.Size(244, 86);
+            this.OyunaBasla_btn.TabIndex = 0;
+            this.OyunaBasla_btn.Text = "Oyuna Başla";
+            this.OyunaBasla_btn.UseVisualStyleBackColor = true;
+            this.OyunaBasla_btn.Click += new System.EventHandler(this.OyunaBasla_btn_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 135);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 199);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(526, 100);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.OyunaBasla_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OyunaBasla_btn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ Adam Asmaca/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void OyunaBasla_btn_Click(object sender, EventArgs e)
+        {
+
+            Random r = new Random();
+            int number = r.Next(0, 4);
+
+
+            string []sehirler={ "İstanbul" , "Ankara" , "İzmir" , "Antalya" };
+            char[] alfabe = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ".ToCharArray();
+            for (int i = 0; i < alfabe.Length; i++)
+            {
+                Button btn = new Button();
+                
+                btn.Text = alfabe[i].ToString();
+                btn.Width = 40;
+                btn.Height = 20;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+            
+
+            for (int i = 0; i < sehirler.Length; i++)
+            {
+                TextBox text = new TextBox();
+            
+                text.Text = sehirler[i].ToString();
+                text.Width = 40;
+                text.Height = 20;
+                flowLayoutPanel2.Controls.Add(text);
+
+            }
+            
+
+        }
+    }
+}
